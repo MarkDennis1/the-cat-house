82a2a73fc2422ebd4d180c3910b7dafac80b6799
MarkDennis1 <markdennissanchez1@gmail.com>
2023-01-02 07:33:53 +0800

    Add controllers

---
+++ app/Http/Controllers/CatController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cat;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as FacadesRequest;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
+
+class CatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $cats = Cat::all();
+        return Inertia::render('Admin/Cat', [
+            'cats' => $cats->map(function ($cat) {
+                return [
+                    'id' => $cat->id,
+                    'name' => $cat->name,
+                    'gender' => $cat->gender,
+                    'age_category' => $cat->age_category,
+                    'tags' => $cat->tags,
+                    'color' => $cat->color,
+                    'image_path' => asset('storage/' . $cat->image_path),
+                ];
+            })
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255',
+            'age_category' => 'required',
+            'tags' => 'required',
+            'color' => 'required',
+        ]);
+
+        $image = '';
+        if (FacadesRequest::file('image_path')) {
+            $image = FacadesRequest::file('image_path')->store('cats', 'public');
+        } else {
+            $image = 'cats/default.png';
+        };
+        $cat = new Cat();
+        $cat->name = $request->name;
+        $cat->gender = $request->gender;
+        $cat->age_category = $request->age_category;
+        $cat->tags = $request->tags;
+        $cat->color = $request->color;
+        $cat->image_path = $image;
+        $cat->save();
+
+        return  redirect()->route('cats.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cat $cat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cat $cat)
+    {
+        //
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cat $cat)
+    {
+        //
+        // dd($request->all());
+        $image = $cat->getAttributes()['image_path'];
+        if (FacadesRequest::file('image_path')) {
+            $image = FacadesRequest::file('image_path');
+        }
+
+        $request->validate([
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255',
+            'age_category' => 'required',
+            'tags' => 'required',
+            'color' => 'required',
+        ]);
+
+        $cat->update([
+            'name' => $request->name,
+            'gender' => $request->gender,
+            'age_category' => $request->age_category,
+            'tags' => $request->tags,
+            'color' => $request->color,
+            'image_path' => $image,
+        ]);
+
+        return  redirect()->route('cats.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cat $cat)
+    {
+        //
+        if ($cat->image_path != 'cats/default.png') {
+            Storage::delete('public/' . $cat->image_path);
+        }
+
+        $cat->delete();
+
+        return  redirect()->route('cats.index');
+    }
+}
---
+++ app/Http/Controllers/VolunteerController.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Volunteer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
+
+class VolunteerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = DB::table('users')->where('id', Auth::id())->first();
+        if ($user->role === 'admin') {
+            $volunteers = Volunteer::all();
+            return Inertia::render('Admin/Volunteer', [
+                'volunteers' => $volunteers->map(function ($volunteer) {
+                    return [
+                        'id' => $volunteer->id,
+                        'first_name' => $volunteer->first_name,
+                        'last_name' => $volunteer->last_name,
+                        'address' => $volunteer->address,
+                        'number' => $volunteer->number,
+                        'occupation' => $volunteer->occupation,
+                        'email' => $volunteer->email,
+                        'age' => $volunteer->age,
+                        'interested_in' => $volunteer->interested_in,
+                        'experience' => $volunteer->experience,
+                    ];
+                })
+            ]);
+        } else {
+            $volunteers = Volunteer::query()->get(['*'])->where('user_id', '=', Auth::id());
+            return Inertia::render('Client/Volunteer', [
+                'volunteers' => $volunteers->map(function ($volunteer) {
+                    return [
+                        'id' => $volunteer->id,
+                        'user_id' => $volunteer->user_id,
+                        'first_name' => $volunteer->first_name,
+                        'last_name' => $volunteer->last_name,
+                        'address' => $volunteer->address,
+                        'number' => $volunteer->number,
+                        'occupation' => $volunteer->occupation,
+                        'email' => $volunteer->email,
+                        'age' => $volunteer->age,
+                        'interested_in' => $volunteer->interested_in,
+                        'experience' => $volunteer->experience,
+                    ];
+                })
+            ]);
+        }
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function client()
+    {
+        return Inertia::render('Volunteer/Index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**x
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //first_name
+        // last_name
+        // address
+        // number
+        // occupation
+        // email
+        // age
+        // interested_in
+        // experience
+        $request->validate([
+            'first_name' => 'required|max:50',
+            'last_name' => 'required|max:50',
+            'address' => 'required|max:100',
+            'number' => 'required|min:11|max:11',
+            'occupation' => 'required|max:50',
+            'email' => 'required',
+            'age' => 'required',
+            'experience' => 'required|max:1000',
+        ]);
+        $volunteer = new Volunteer();
+        $volunteer->user_id = Auth::id();
+        $volunteer->first_name = $request->first_name;
+        $volunteer->last_name = $request->last_name;
+        $volunteer->address = $request->address;
+        $volunteer->number = $request->number;
+        $volunteer->occupation = $request->occupation;
+        $volunteer->email = $request->email;
+        $volunteer->age = $request->age;
+        $volunteer->interested_in = $request->json_interested_in;
+        $volunteer->experience = $request->experience;
+        $volunteer->save();
+
+        return  redirect()->route('volunteers.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Volunteer  $volunteer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Volunteer $volunteer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Volunteer  $volunteer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Volunteer $volunteer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Volunteer  $volunteer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Volunteer $volunteer)
+    {
+        //
+        $request->validate([
+            'first_name' => 'required|max:50',
+            'last_name' => 'required|max:50',
+            'address' => 'required|max:100',
+            'number' => 'required|max:11',
+            'occupation' => 'required|max:50',
+            'email' => 'required',
+            'age' => 'required',
+            'experience' => 'required|max:1000',
+        ]);
+
+        $volunteer->update([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'address' => $request->address,
+            'number' => $request->number,
+            'occupation' => $request->occupation,
+            'email' => $request->email,
+            'age' => $request->age,
+            'interested_in' => $request->json_interested_in,
+            'experience' => $request->experience,
+        ]);
+
+        return  redirect()->route('volunteers.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Volunteer  $volunteer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Volunteer $volunteer)
+    {
+        //
+        $volunteer->delete();
+
+        return  redirect()->route('volunteers.index');
+    }
+}
